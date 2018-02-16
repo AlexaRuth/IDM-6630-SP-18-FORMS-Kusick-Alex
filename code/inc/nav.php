028f9c2a8461614b3fcc063b9c8e218dfd04793d
Alex Kusick <Akusick@mcad.edu>
2018-02-16 01:09:08 -0600

    nav update

--- code/inc/nav.php
+++ code/inc/nav.php
@@ -3,12 +3,12 @@
     <a href="/">Home</a>
   </li>
   <li>
-    <a href="/delete.php">Delete</a>
+    <a href="/Update.php">Update</a>
+  </li>
+  <li>
+   <a href="/delete.php">Delete</a>
   </li>
   <li>
    <a href="/Insert.php">Insert</a>
-  </li>
-  <li>
-   <a href="/Update.php">Update</a>
   </li>
 </ul>
